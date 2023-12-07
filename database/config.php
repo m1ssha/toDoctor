c253de17d274ed212a52560f49d35bfd4a96683c
m1ssha <m1ssha@vk.com>
2023-12-07 13:47:12 +0300

    commit

--- database/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Конфиг для настройки доступа к базе данных и бота Telegram для авторизации
-$BOT_TOKEN = "";
-$BOT_USERNAME = "";
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "toDoctor";
